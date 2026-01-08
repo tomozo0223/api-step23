50da1fa169cdf87a31d34d729c81085fe3cc65ad
tomoki <tmdesign.0223@gmail.com>
2026-01-08 22:43:06 +0900

    データ取得と表示処理追加

---
+++ app.js
@@ -0,0 +1,31 @@
+const btn = document.getElementById("js-btn");
+const result = document.getElementById("js-result");
+
+const getData = async (name) => {
+  try {
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+    if (!res.ok) {
+      result.innerHTML = `
+        <p>正しいポケモンの名前を入力してください。</p>
+      `;
+      throw new Error("正しいポケモンの名前を入力してください。");
+    }
+    const data = await res.json();
+    displayData(data);
+  } catch (error) {
+    console.log("エラー発生", error.message);
+  }
+};
+
+const displayData = (data) => {
+  result.innerHTML = `
+    <div>名前: ${data.name}</div>
+    <img src="${data.sprites.front_default}">
+    <div>タイプ: ${data.types[0].type.name}</div>
+  `;
+};
+
+btn.addEventListener("click", () => {
+  const name = document.getElementById("js-name").value;
+  getData(name);
+});
